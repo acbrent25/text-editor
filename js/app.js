53df2eb4357c61c20c85630402ee9c569fbb12a2
acbrent25 <adamchampagne@gmail.com>
2019-07-31 14:26:32 -0400

    remove dashes

--- js/app.js
+++ js/app.js
@@ -20,6 +20,16 @@ $(document).ready(function(){
         $("#text-edit-area").val(formattedText);
     });
 
+    // Remove Dashes
+    $("#remove-dashes").on('click', function(){
+        // get text from text area
+        let textToFormat = $("#text-edit-area").val();
+        // format the text and set new variable
+        let formattedText = textToFormat.replace(new RegExp('-', 'g')," ")
+        // replace text for copying
+        $("#text-edit-area").val(formattedText);
+    });
+
 
     
 
